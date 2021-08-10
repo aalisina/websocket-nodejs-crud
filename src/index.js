22f69f14ddef7cfa8219ac0967f5f0018ba4bcac
sinazay123 <sinazay123@gmail.com>
2021-08-10 17:13:41 -0500

    agregar eventos en el servidor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { Server as WebSocketServer } from 'socket.io';
 import http from 'http';
+import { v4 as uuid } from 'uuid';
 
 const notes = [];
 const app = express();
@@ -11,10 +12,16 @@ app.use(express.static(`${__dirname}/public`));
 
 io.on('connection', (socket) => {
   console.log('new connection', socket.id);
+  socket.emit('server:loadnotes', notes);
 
   // guardar data del evento client:newnote
-  socket.on('client:newnote', (data) => {
-    notes.push(data);
+  socket.on('client:newnote', (newNote) => {
+    const note = {
+      ...newNote,
+      id: uuid(),
+    };
+    notes.push(note);
+    socket.emit('server:newnote', newNote);
   });
 });
 
