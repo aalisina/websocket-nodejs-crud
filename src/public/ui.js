f624cf775f6a80eb88b9f29531bab3df7611d0db
sinazay123 <sinazay123@gmail.com>
2021-08-10 17:14:55 -0500

    agregar archivo de ui

---
+++ src/public/ui.js
@@ -0,0 +1,22 @@
+const noteslist = document.querySelector('#notes');
+
+const appendNote = (note) => {
+  noteslist.innerHTML += `
+  <div class="card card-body rounded-0 mb-2">
+      <div class="d-flex justify-content-between">
+        <h1 class="h3 card-title">${note.title}</h1>
+        <div>
+          <button class="btn btn-danger">Delete</button>
+          <button class="btn btn-secondary">Update</button>
+        </div>
+      </div>
+          <p>${note.description}</p>
+  </div>
+  `;
+};
+
+const loadNotes = (notes) => {
+  notes.forEach((note) => {
+    appendNote(note);
+  });
+};
