f26805e4ec0c682d08759c7d83bc95a7cd785476
sinazay123 <sinazay123@gmail.com>
2021-08-10 17:14:40 -0500

    agregar archivo de socket para manejar los eventos del cliente

---
+++ src/public/sockets.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+const socket = io();
+
+const saveNote = (title, description) => {
+  socket.emit('client:newnote', {
+    title,
+    description,
+  });
+};
+
+socket.on('server:newnote', appendNote);
+
+socket.on('server:loadnotes', loadNotes);
